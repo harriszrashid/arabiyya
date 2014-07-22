75ad4987e32ca5251f117e25ac9edbbd0ac7bca5
Harris Rashid <harris@chapterthree.com>
2014-07-21 18:47:12 -0700

    updating styling

--- sites/all/themes/arabiyya/js/arabiyya.behaviors.js
+++ sites/all/themes/arabiyya/js/arabiyya.behaviors.js
@@ -21,16 +21,21 @@
 (function($) {
   Drupal.behaviors.arabiyyaShowHide = {
     attach: function(context, settings) {
+      $(".flexslider li .views-field.views-field-title-field-1 h2").addClass("active-slide");
+
       $(".flexslider li .views-field h2").click(function(e) {
-        $(this).fadeTo(100, 0).css("display", "none");
+        $(this).fadeTo(300, 0).css("display", "none").removeClass("active-slide");
         var $next = $(this).parent().next().find("h2");
 
+        //If it's the last field return to the first div
         if($(this).parent().hasClass('views-field-title')){
           //alert('hey');
-          $(this).parent().parent().find(".views-field:first-child h2").fadeTo(300, 1).css("display", "block");
+          $(this).parent().parent().find(".views-field:first-child h2").fadeTo(300, 1).css("display", "block").addClass("active-slide");
+
         }
+        //Go to the next div, fade in.
         else {
-          $next.stop().fadeTo(300, 1).css("display", "block");
+          $next.stop().fadeTo(300, 1).css("display", "block").addClass("active-slide");
         }
         e.preventDefault();
       });
